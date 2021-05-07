e1bc318f330bb21e88d4e75cebbd3abf2e3fa2a5
georgecroz1 <83789983+georgecroz1@users.noreply.github.com>
2021-05-07 10:32:33 +0100

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,14 @@
+function check() {
+    var c=0;
+    var q1=document.quiz.question1.value;
+    if (q1=="C. asking for personal information & Suspicious links") {c++}
+        document.write(c);
+}
+
+
+
+
+
+
+
+
